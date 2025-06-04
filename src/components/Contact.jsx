0f733484b812f28eebe28be00daefa0c447a2dd5
Tejas Gawade <tgawade092@gmail.com>
2025-06-04 14:32:32 +0530

    Configure EmailJS for contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import { motion } from 'framer-motion';
 import { FaEnvelope, FaLinkedin, FaGithub } from 'react-icons/fa';
 import { FaXTwitter } from "react-icons/fa6";
+import emailjs from '@emailjs/browser';
 
 const socials = [
   { icon: <FaEnvelope />, label: 'Email', url: 'mailto:tgawade092@gmail.com' },
@@ -10,6 +12,40 @@ const socials = [
 ];
 
 const Contact = () => {
+  const [submitting, setSubmitting] = useState(false);
+
+  // Initialize EmailJS with the public key
+  emailjs.init({ 
+      publicKey: import.meta.env.VITE_EMAIL_PUBLIC_KEY,
+      limitRate:{
+        id: 'contact-form',
+        throttle: 10000, // allow 1 request per 10seconds
+      }
+  })
+
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+    emailjs.sendForm(
+      import.meta.env.VITE_EMAIL_SERVICE_ID,
+      import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+      e.target,
+    ).then(
+      (response) => {
+        alert('Message sent successfully!');
+        console.log('Message sent successfully:', response);
+        setSubmitting(false);
+      },
+      (error) => {
+        alert('Failed to send message. Please try again later.');
+        console.error('Error sending message:', error);
+        setSubmitting(false);
+      }
+    );
+    e.target.reset(); // Reset the form after submission
+  };
+
   return (
     <section id="contact" className="min-h-screen flex flex-col justify-center items-center py-20 bg-gradient-to-br from-white via-slate-100 to-slate-200 dark:from-gray-950 dark:via-gray-900 dark:to-gray-950 relative">
       <motion.h2
@@ -29,21 +65,24 @@ const Contact = () => {
             </a>
           ))}
         </div>
-        <form className="flex flex-col gap-4 bg-white/60 dark:bg-gray-950/80 p-8 rounded-2xl shadow-lg w-full md:w-2/3">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 bg-white/60 dark:bg-gray-950/80 p-8 rounded-2xl shadow-lg w-full md:w-2/3">
           <motion.input
             whileFocus={{ scale: 1.04 }}
+            name='name'
             type="text"
             placeholder="Your Name"
             className="px-4 py-3 rounded-lg text-neutral-900 dark:text-neutral-200 placeholder:text-neutral-600 dark:placeholder:text-neutral-500 border border-blue-900 focus:outline-none focus:ring-2 focus:ring-blue-900 transition-all"
           />
           <motion.input
             whileFocus={{ scale: 1.04 }}
+            name='email'
             type="email"
             placeholder="Your Email"
             className="px-4 py-3 rounded-lg text-neutral-900 dark:text-neutral-200 placeholder:text-neutral-600 dark:placeholder:text-neutral-500 border border-blue-900 focus:outline-none focus:ring-2 focus:ring-blue-900 transition-all"
           />
           <motion.textarea
             whileFocus={{ scale: 1.04 }}
+            name='message'
             rows="5"
             placeholder="Your Message"
             className="px-4 py-3 rounded-lg text-neutral-900 dark:text-neutral-200 placeholder:text-neutral-600 dark:placeholder:text-neutral-500 border border-blue-900 focus:outline-none focus:ring-2 focus:ring-blue-900 transition-all resize-none"
@@ -51,9 +90,10 @@ const Contact = () => {
           <motion.button
             whileHover={{ scale: 1.05 }}
             type="submit"
-            className="mt-2 px-6 py-3 rounded-full bg-gradient-to-r from-cyan-700 via-blue-700 to-indigo-700 text-white font-bold shadow-lg hover:shadow-xl transition-transform duration-200"
+            className="mt-2 px-6 py-3 rounded-full bg-gradient-to-r from-cyan-700 via-blue-700 to-indigo-700 text-white font-bold shadow-lg hover:shadow-xl transition-transform duration-200 hover:cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+            disabled={submitting}
           >
-            Send Message
+            {submitting ? 'Sending...':'Send Message'}
           </motion.button>
         </form>
       </div>
